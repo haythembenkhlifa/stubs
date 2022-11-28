a9135e7c4030d7344c6b57c6697a66122a48ee26
Haythem Ben Khlifa <benkhlifahaythem@gmail.com>
2022-11-28 11:53:21 +0200

    first commit

---
+++ app/Products/Momentum.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Products;
+
+class Momentum
+{
+
+
+    /**
+     * Get product details.
+     * 
+     * @return void
+     */
+    public function getProduct()
+    {
+        //
+    }
+
+
+    /**
+     * Set product details.
+     * 
+     * @return void
+     */
+    public function setProduct()
+    {
+        //
+    }
+}
